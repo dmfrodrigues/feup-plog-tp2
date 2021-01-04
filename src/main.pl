9da832fdbfbe075d170f12b4f814d50df72db34f
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-01-04 20:11:05 +0000

    Moved stats

--- src/main.pl
+++ src/main.pl
@@ -56,16 +56,6 @@ write_sol(solution(ID, [A|Allocation])) :-
     format('~d ', A),
     write_sol(solution(ID, Allocation)).
 
-print_statistics_header :- format('Resumptions~11+,Entailments~12+,Prunings~9+,Backtracks~11+,Constraints~12+,Total_runtime~14+~n', []).
-print_statistics :-
-    fd_statistics(resumptions, Resumptions),
-    fd_statistics(entailments, Entailments),
-    fd_statistics(prunings   , Prunings   ),
-    fd_statistics(backtracks , Backtracks ),
-    fd_statistics(constraints, Constraints),
-    statistics(total_runtime, [_, Total_runtime]),
-    format('~d~11+,~d~12+,~d~9+,~d~11+,~d~12+,~d~14+~n', [Resumptions, Entailments, Prunings, Backtracks, Constraints, Total_runtime]).
-
 :-
     % Classes = [
     %     class(1, 1, []),
---
+++ stats/stats.pl
@@ -0,0 +1,9 @@
+print_statistics_header :- format('Resumptions~11+,Entailments~12+,Prunings~9+,Backtracks~11+,Constraints~12+,Total_runtime~14+~n', []).
+print_statistics :-
+    fd_statistics(resumptions, Resumptions),
+    fd_statistics(entailments, Entailments),
+    fd_statistics(prunings   , Prunings   ),
+    fd_statistics(backtracks , Backtracks ),
+    fd_statistics(constraints, Constraints),
+    statistics(total_runtime, [_, Total_runtime]),
+    format('~d~11+,~d~12+,~d~9+,~d~11+,~d~12+,~d~14+~n', [Resumptions, Entailments, Prunings, Backtracks, Constraints, Total_runtime]).
