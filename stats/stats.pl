69e08577681c92a3cc9bc277739fa73ac7f182e7
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-01-05 19:34:12 +0000

    Minor changes

--- stats/stats.pl
+++ stats/stats.pl
@@ -20,6 +20,15 @@ print_statistics(Value) :-
     fd_statistics(constraints, Constraints),
     statistics(total_runtime, [_, Total_runtime]),
     format('~d,~d,~d,~d,~d,~d,~d~n', [Resumptions, Entailments, Prunings, Backtracks, Constraints, Total_runtime, Value]).
+print_statistics(Value, N) :-
+    fd_statistics(resumptions, Resumptions),
+    fd_statistics(entailments, Entailments),
+    fd_statistics(prunings   , Prunings   ),
+    fd_statistics(backtracks , Backtracks ),
+    fd_statistics(constraints, Constraints),
+    statistics(total_runtime, [_, Total_runtime]),
+    Total_runtimeN is Total_runtime/N,
+    format('~d,~d,~d,~d,~d,~d,~d~n', [Resumptions, Entailments, Prunings, Backtracks, Constraints, Total_runtimeN, Value]).
 
 repeatN(0, _) :- !.
 repeatN(N, T) :- T, N1 is N-1, repeatN(N1, T).
