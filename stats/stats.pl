e6a5c28007759f46b9b834c8e6990c3ecdf914e8
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-01-05 19:14:41 +0000

    Changes

--- stats/stats.pl
+++ stats/stats.pl
@@ -1,8 +1,8 @@
 print_statistics_header :-
-    format('Resumptions~40+,Entailments~15+,Prunings~15+,Backtracks~15+,Constraints~15+,Total_runtime (ms)~25+~n', []),
+    format('Resumptions,Entailments,Prunings,Backtracks,Constraints,TotalRuntime~n', []),
     statistics(total_runtime, _).
 print_statistics_header(_) :-
-    format('Resumptions~40+,Entailments~15+,Prunings~15+,Backtracks~15+,Constraints~15+,Total_runtime (ms)~25+,Value~10+~n', []),
+    format('Resumptions,Entailments,Prunings,Backtracks,Constraints,TotalRuntime,Value~n', []),
     statistics(total_runtime, _).
 print_statistics :-
     fd_statistics(resumptions, Resumptions),
@@ -11,7 +11,7 @@ print_statistics :-
     fd_statistics(backtracks , Backtracks ),
     fd_statistics(constraints, Constraints),
     statistics(total_runtime, [_, Total_runtime]),
-    format('~d~40+,~d~15+,~d~15+,~d~15+,~d~15+,~d~25+~n', [Resumptions, Entailments, Prunings, Backtracks, Constraints, Total_runtime]).
+    format('~d,~d,~d,~d,~d,~d~n', [Resumptions, Entailments, Prunings, Backtracks, Constraints, Total_runtime]).
 print_statistics(Value) :-
     fd_statistics(resumptions, Resumptions),
     fd_statistics(entailments, Entailments),
@@ -19,7 +19,7 @@ print_statistics(Value) :-
     fd_statistics(backtracks , Backtracks ),
     fd_statistics(constraints, Constraints),
     statistics(total_runtime, [_, Total_runtime]),
-    format('~d~40+,~d~15+,~d~15+,~d~15+,~d~15+,~d~25+,~d~10+~n', [Resumptions, Entailments, Prunings, Backtracks, Constraints, Total_runtime, Value]).
+    format('~d,~d,~d,~d,~d,~d,~d~n', [Resumptions, Entailments, Prunings, Backtracks, Constraints, Total_runtime, Value]).
 
 repeatN(0, _) :- !.
 repeatN(N, T) :- T, N1 is N-1, repeatN(N1, T).
